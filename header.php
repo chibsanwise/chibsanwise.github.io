7e121d90458b47dc0861cc8641934ddd6a04e26b
chibsanwise <chibale.anwisye@gmail.com>
2018-09-01 22:00:35 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,64 @@
+<?php
+include'conn.php'
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8" />
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <title>Perpetual Chaos</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+ <link rel="stylesheet" type="text/css" media="screen" href="931.css" />
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+ <script src="931.js"></script>
+ <link rel="icon" type="image/png" href="pics/931.ico">
+
+ <style>
+ .footer{
+ bottom: 0;
+ color: azure;
+ right: 0;
+ left: 0;
+ }
+ .dscrb{
+ height: 200px;
+ width: 300px;
+ }
+ .inputfld{
+ width: 200px;
+ }
+ </style>
+</head>
+</html>
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+     <a class="navbar-brand" href="index.php"><img src="pics/931 logo.png" alt="931 logo"></a>
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+ <span class="navbar-toggler-icon"></span>
+ </button>
+ 
+ <div class="collapse navbar-collapse" id="navbarSupportedContent">
+ <ul class="navbar-nav mr-auto">
+ 
+ <li class="nav-item"  style="color:rgb(0,254,0);">
+     <a class="nav-link" href="Robopage.php">Robots</a>
+ </li>
+ <li class="nav-item " style="color:rgb(0,254,0);">
+     <a class="nav-link" href="teammembers.php" >
+ Team
+ </a>
+ </li>
+ <li class="nav-item" style="color:rgb(0,254,0);">
+ <a class="nav-link " href="#">Donations</a>
+ </li>
+ </ul>
+ 
+ </div>
+ </nav>
