127d9c116f2806406d5e8b5ad5554fdc1600acd2
chibsanwise <chibale.anwisye@gmail.com>
2018-09-01 22:02:11 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+ include 'header.php';?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+include 'Body.php';?>
+        
+<?php include 'newsandthings.php'?>
+    </body>
+
+<?php
+include 'footer.php';
